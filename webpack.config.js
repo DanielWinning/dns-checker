f0627c32ff9de23eb89f2c0daa713dcc09278850
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-15 12:37:31 +0100

    added typescript and watch command

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: './assets/js/app.js',
+    entry: './assets/js/app.ts',
+    devtool: 'inline-source-map',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'public/assets/js')
