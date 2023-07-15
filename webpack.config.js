6235d541b1c0e78da3b4cc973919fa6b737a1ee9
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-15 13:01:19 +0100

    added scss

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
