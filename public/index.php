bc6d78cf51371a12750265b51e4d5239ff2a69ca
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-07-16 02:23:28 +0100

    removed unused var

--- public/index.php
+++ public/index.php
@@ -8,8 +8,6 @@ require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__, 1));
 $viewsPath = sprintf('%s/views', dirname(__DIR__, 1));
 $requestParser = new QueryParser($_SERVER['REQUEST_URI']);
 
-$requestUri = trim(strip_tags($_SERVER['REQUEST_URI']), '/');
-
 if (empty($requestParser->getUri()) && empty($requestParser->getQueryParams())) {
     require_once sprintf('%s/index.view.html', $viewsPath);
     return;
